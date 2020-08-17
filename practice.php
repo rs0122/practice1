00537042d6c278cce8900e7f0a4adc36323a0bd3
斉藤 <rs.0122.ssthe2@hotmail.co.jp>
2020-08-17 03:05:57 +0000

    practice2

--- practice.php
+++ practice.php
@@ -29,3 +29,34 @@ $calendar_2018 = [
 
 // 12月を表示する
 echo $calendar_2018["December"];
+$name = "斉藤";
+if ($name == "斉藤") {
+    echo "私は斉藤です" ;
+}else{
+    echo "斉藤ではありません";
+}
+
+$total = 0;
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+
+echo $total;
+$fruits = array("apple", "orange", "lemon", "banana", "melon");
+foreach($fruits as $fruit){
+    echo $fruit;
+    echo "\n";
+}
+/* for文の始めの値を定義する */
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
